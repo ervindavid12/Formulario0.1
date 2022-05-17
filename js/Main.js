b6cfaa4f1ba0922364b1c3f66c2cfd56674d6d62
ervindavid12 <103067788+ervindavid12@users.noreply.github.com>
2022-05-17 13:42:59 -0600

    Add files via upload

---
+++ js/Main.js
@@ -0,0 +1,44 @@
+function recuperar_datos() {
+    var user = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var nombre 
+   
+    if(user=='David2004'){ 
+        alert('Youre username is correct');
+        if (password=="maxiboy100"){
+            alert('Youre password is correct');
+        }else{
+            alert('Youre password is incorrect');
+        }
+    }else{
+        alert('Youre username is incorrect');
+        nombre=false
+        if(nombre== true){
+            
+        }else{
+            alert('Youre password is incorrect');
+        }
+    }
+    
+    if(user=='Carlos2005'){ 
+        alert('Youre username is correct');
+        if (password=="Maximas123"){
+            alert('Youre password is correct');
+        }else{
+            alert('Youre password is incorrect');
+        }
+    }else{
+        alert('Youre username is incorrect');
+        nombre=false
+        if(nombre== true){
+            
+        }else{
+            alert('Youre password is incorrect');
+        }
+    }
+
+    
+}
+
+
+
